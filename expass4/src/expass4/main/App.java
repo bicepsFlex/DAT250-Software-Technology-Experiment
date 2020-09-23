362e7f696fac65f4ed61959fa9103c687e959b16
Jaroslaw Pasiak <wojapa@gmail.com>
2020-09-23 20:32:18 +0200

    Experiment 2 done :)

--- expass4/src/expass4/main/App.java
+++ expass4/src/expass4/main/App.java
@@ -4,6 +4,16 @@ import static spark.Spark.after;
 import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.put;
+import static spark.Spark.post;
+import static spark.Spark.delete;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import com.google.gson.Gson;
 
@@ -12,6 +22,8 @@ import expass4.model.Todo;
 public class App {
 	
 	static Todo todos = null;
+    private static final String PERSISTENCE_UNIT_NAME = "todos";
+    private static EntityManagerFactory factory;
 	
 	public static void main(String[] args) {
 
@@ -27,16 +39,89 @@ public class App {
   		  res.type("application/json");
   		});
 		
-        get("/todos", (req, res) -> todos.toJson());
-               
-        put("/todos", (req,res) -> {
-        
+        get("/todos", (req, res) ->{
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            EntityManager em = factory.createEntityManager();
+            Query q = em.createQuery("select t from Todo t");
+            List<Todo> todos = q.getResultList();
         	Gson gson = new Gson();
-        	
-        	todos = gson.fromJson(req.body(), Todo.class);
+        	String todosString = gson.toJson(todos);
+            return todosString;
+        });
+               
+        put("/todos/:id", (req,res) -> {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            EntityManager em = factory.createEntityManager();
+        	try {
+				Gson gson = new Gson();
+				todos = gson.fromJson(req.body(), Todo.class);
+				em.getTransaction().begin();
+				Query q = em.createQuery("select t from Todo t WHERE t.id = :id");
+				q.setParameter("id", Long.parseLong(req.params(":id")));
+				Todo todo = (Todo) q.getSingleResult();
+				if(todos.getSummary() == null && todos.getDescription() == null) {
+					res.status(202);
+					return "202 No changes specified, are you sure?";
+				} else {
+					if(todos.getSummary() != null) {
+						todo.setSummary(todos.getSummary());
+					}
+					if(todos.getDescription() != null) {
+						todo.setDescription(todos.getDescription());
+					}
+				}
+				em.persist(todo);
+				em.getTransaction().commit();
+				em.close();
+				return todo.toJson();
+			} catch (NoResultException e) {
+				res.status(404);
+				return "404 Try Again Later :)";
+			}
+        	});
         
-            return todos.toJson();
-        	
+        post("/todos", (req,res) -> {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            EntityManager em = factory.createEntityManager();
+			Gson gson = new Gson();
+			todos = gson.fromJson(req.body(), Todo.class);
+			em.getTransaction().begin();
+			Todo todo = new Todo();
+			if(todos.getSummary() == null && todos.getDescription() == null) {
+				res.status(202);
+				return "202 no changes specified\nAre you doing it right?";
+			} else { 
+				if(todos.getSummary() != null) {
+					todo.setSummary(todos.getSummary());
+				}
+				if(todos.getDescription() != null) {
+					todo.setDescription(todos.getDescription());
+				}
+				em.persist(todo);
+				em.getTransaction().commit();
+				em.close();
+			}
+			return todo.toJson();
+			});
+        
+        delete("/todos/:id", (req,res) -> {
+            factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            EntityManager em = factory.createEntityManager();
+			try {
+				String id = req.params(":id");
+				em.getTransaction().begin();
+				Query q = em.createQuery("select t from Todo t WHERE t.id = :id");
+				q.setParameter("id", Long.parseLong(id));
+				Todo todo = (Todo) q.getSingleResult();
+				Query q1 = em.createQuery("DELETE FROM Todo t WHERE t.id = :id");
+				q1.setParameter("id", Long.parseLong(id)).executeUpdate();
+				em.getTransaction().commit();
+				em.close();
+				return "Successfully Deleted:\n"+ todo.toJson() +"\nHave a Nice day";
+			} catch (NoResultException e) {
+				res.status(404);
+				return "404 Wait What :O";
+			}
         });
     }
     
